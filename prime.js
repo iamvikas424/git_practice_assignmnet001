3f8f33b113ec878ab32b940def79fe0a40e00d76
iamvikas424 <vchoudhary2120@gamil.com>
2022-09-20 18:51:44 +0530

    code updated by vikas

--- prime.js
+++ prime.js
@@ -1,7 +1,18 @@
-let num=15;
-for(let i=i;i<=num;i++){
-if(num%i==0){
-count++;
+function checkprime(num){
+if(num<=1|| num==undefined){
+return false;
 }
+for(let i=2;i<=num**(0.5);i++){
+if(num%i==0){
+return false;
 }
 
+}
+return true;
+}
+let x=checkprime();
+if(x==true){
+console.log(num,"is prime number")
+}else{
+console.log(num,"is not prime number")
+}
